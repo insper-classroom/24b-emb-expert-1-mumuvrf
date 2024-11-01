bb78d1af5db2a6bb3738e4199c59f0c41a67a1d8
mumuvrf <viniciusrf2@al.insper.edu.br>
2024-11-01 17:23:42 -0300

    Add FreeRTOS

--- main/main.c
+++ main/main.c
@@ -3,15 +3,30 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
-
-#include <stdio.h>
 #include "pico/stdlib.h"
+#include <stdio.h>
+
+#include <FreeRTOS.h>
+#include <task.h>
+#include <semphr.h>
+#include <queue.h>
+
 #include "hardware/gpio.h"
+
+void microphone_task(void *p){
+    while(true){
+        printf("Hello World!!!\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
 
 int main() {
     stdio_init_all();
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
-    }
+
+    xTaskCreate(microphone_task, "Microphone Task", 8192, NULL, 1, NULL);
+
+    vTaskStartScheduler();
+
+    while (true)
+        ;
 }
